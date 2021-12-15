59d016024fdc93506a03bae5d08f24e91f1d1bbd
Khmel Mykyta <hmelenok@gmail.com>
2021-12-15 19:33:37 +0200

    feat:  initial commit + tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,86 @@
-import {getFoo} from './index';
+import {queueRequest} from './index';
 
-it('should return bar', () => {
-  expect(getFoo()).toEqual('bar');
+describe('#queueRequest', () => {
+  it('we should call multiple Promise functions from 1 variable', async () => {
+    const apiMock = jest.fn();
+    const APIRequest = async (params: number) =>
+      new Promise<any>(resolve => {
+        {
+          setTimeout(() => {
+            resolve(apiMock(params));
+          }, 100);
+        }
+      });
+    await Promise.all(
+      [
+        {call: APIRequest, params: 1},
+        {call: APIRequest, params: 2},
+        {call: APIRequest, params: 3},
+        {call: APIRequest, params: 4},
+      ].map(({call, params}) => call(params))
+    );
+
+    expect(apiMock).toHaveBeenCalledTimes(4);
+  });
+
+  it('should call call API Promise only once', async () => {
+    const apiMock = jest.fn();
+    const APIRequest = async (params: number) =>
+      new Promise<any>(resolve => {
+        {
+          setTimeout(() => {
+            resolve(apiMock(params));
+          }, 100);
+        }
+      });
+    await Promise.all([
+      queueRequest({request: APIRequest, params: 1}),
+      queueRequest({request: APIRequest, params: 2}),
+      queueRequest({request: APIRequest, params: 3}),
+      queueRequest({request: APIRequest, params: 4}),
+    ]);
+
+    expect(apiMock).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call call API Promise twice as timer ended', async () => {
+    const apiMock = jest.fn();
+    const APIRequest = async (params: number) =>
+      new Promise<any>(resolve => {
+        {
+          setTimeout(() => {
+            resolve(apiMock(params));
+          }, 0);
+        }
+      });
+    await Promise.all([
+      queueRequest({request: APIRequest, params: 1, processDelay: 90}),
+      queueRequest({request: APIRequest, params: 2}),
+      queueRequest({request: APIRequest, params: 3}),
+      queueRequest({request: APIRequest, params: 4}),
+    ]);
+
+    await new Promise<any>(resolve => {
+      {
+        setTimeout(() => {
+          resolve(queueRequest({request: APIRequest, params: 5}));
+        }, 100);
+      }
+    });
+
+    await new Promise<any>(resolve => {
+      {
+        setTimeout(() => {
+          //@ts-ignore
+          const stack = new Promise.all([
+            queueRequest({request: APIRequest, params: 6}),
+            queueRequest({request: APIRequest, params: 7}),
+          ]);
+          resolve(stack);
+        }, 100);
+      }
+    });
+
+    expect(apiMock).toHaveBeenCalledTimes(3);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,83 @@
-export function getFoo(): string {
-  return 'bar';
-}
+/**
+ * Collector for params
+ */
+const paramsMap = new Map();
+/**
+ * Set of requests:
+ * - Easy to identify function signature
+ */
+const requestSet = new Set();
+/**
+ * Map or promises:
+ * - Unification of Promise to single point
+ */
+const promiseMap = new Map();
+
+export const queueRequest = async <T>({
+  request,
+  params,
+  processDelay,
+}: {
+  request: (params: T) => Promise<any>;
+  params: any;
+  processDelay?: number;
+}) => {
+  /**
+   * Assign params to Map by request
+   */
+  paramsMap.set(request, [...(paramsMap.get(request) || []), params]);
+
+  /**
+   * Check requestSet to existing function signature
+   */
+  if (!requestSet.has(request)) {
+    /**
+     * Create Promise pointer
+     */
+    const ongoingRequest = new Promise((resolve, reject) => {
+      /**
+       * Add request signature to Set
+       */
+      requestSet.add(request);
+
+      /**
+       * Put invocation to end of stack
+       */
+      setTimeout(() => {
+        try {
+          /**
+           * Invoke Promise request after timer expiration
+           * with params from paramsMap
+           */
+          resolve(request(paramsMap.get(request)));
+        } catch (ex) {
+          reject(ex);
+        }
+        /**
+         * Cleanup: batch of changes ended
+         * - remove request, params, promise
+         */
+        requestSet.delete(request);
+        paramsMap.delete(request);
+        promiseMap.delete(request);
+      }, processDelay); /* Can be 0 - end of stack or any time */
+    });
+
+    /**
+     * Add Promise sync. in map for next invocation in stack
+     */
+    promiseMap.set(request, ongoingRequest);
+
+    /**
+     * return Promise pointer
+     */
+    return ongoingRequest;
+  }
+
+  /**
+   * return prev Promise pointer if request signature detected
+   */
+  return promiseMap.get(request);
+};
+
+export default queueRequest;
